04173d92e2ea0316589569b6cb92475e1f2df28c
linhbuitung <linh.buitung18@gmail.com>
2024-03-11 14:50:42 +0100

    added answers to task 1

--- LAB2/main.py
+++ LAB2/main.py
@@ -31,10 +31,10 @@ Write a Python program that takes input from the user to create a list of intege
 input_numbers = input("Enter a series of space-separated integers: ")
 
 # Convert Input:
-
+numbers_list = list(map(int, input_numbers.split())) 
 
 # Sorting:
-
+numbers_list.sort()
 
 # Print Output
 print("Sorted list:", numbers_list)
